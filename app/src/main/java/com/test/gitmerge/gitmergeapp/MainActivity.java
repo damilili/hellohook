4e87787597871a20611df2606ffa9bcd2b83fa3b
褚大明 <chudaming213>
2019-07-17 16:11:36 +0800

    MainActivity
    修改打印

--- app/src/main/java/com/test/gitmerge/gitmergeapp/MainActivity.java
+++ app/src/main/java/com/test/gitmerge/gitmergeapp/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("adb");
+        System.out.println("adbed");
     }
 }
