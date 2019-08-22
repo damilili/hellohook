337deec33860bedf7d9736321b4f131224251e4f
chudaming <daming.chu@kuwo.cn>
2019-08-22 17:28:27 +0800

    Updateef MainActivity.java

--- app/src/main/java/com/test/gitmerge/gitmergeapp/MainActivity.java
+++ app/src/main/java/com/test/gitmerge/gitmergeapp/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("159357");
+        System.out.println("abc");
     }
 }
