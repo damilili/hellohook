d66e4f606231772b6e89d87e28428ff3c71eb63e
褚大明 <chudaming213>
2019-10-12 11:35:32 +0800

    afefaca

--- app/src/main/java/com/test/gitmerge/gitmergeapp/MainActivity.java
+++ app/src/main/java/com/test/gitmerge/gitmergeapp/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        System.out.println("eeeceee");
+        System.out.println("eeeceeeee");
     }
 }
