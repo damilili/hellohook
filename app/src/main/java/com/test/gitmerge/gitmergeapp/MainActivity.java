25b8ad018f2d7972f47a555c1d9797872453ec7a
褚大明 <chudaming213>
2019-10-12 13:59:03 +0800

    afeb

--- app/src/main/java/com/test/gitmerge/gitmergeapp/MainActivity.java
+++ app/src/main/java/com/test/gitmerge/gitmergeapp/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+fa
         setContentView(R.layout.activity_main);
         System.out.println("g,ei,bsrbny");
     }
